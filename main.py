aca41f392f39594a514326bf4882fc4f71e9264a
Jaspe Michael <micaeljaspe@gmail.com>
2025-11-17 19:09:28 +0100

    fix: filename issues

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def choisir_fichier() -> str | None:
     else:
         return None
 
-def choisir_fichier() -> str | None:
+
+def choisir_fichier2() -> str | None:
     global file_output
-    
     filepath = filedialog.askopenfilename(
         title="Choisir un fichier",
         filetypes=[("Fichier excel", "*.xlsx")]
@@ -32,18 +32,20 @@ def choisir_fichier() -> str | None:
     if filepath:
         file = filepath
         file_output = file
-        return filepath
+        return file_output
     else:
         return None
 
+
 def completer_action():
     messagebox.showinfo("Action", "Bouton 'Completer' pressé !")
+    print(file_input, file_output)
     current_path = Path('.').absolute()
-    assets_path = current_path.join("assets").join("coordinates.csv")
+    assets_path = current_path.joinpath("assets").joinpath("coordinates.csv")
 
-    c = Converter(file, str(assets_path))
-    print(c.reader.map)
-    print(c.get_from_to())
+    c = Converter(file_input, file_output, str(assets_path))
+    c.reader.map
+    c.get_from_to()
     c.loader.close()
     # Add logic around here
 
@@ -58,7 +60,7 @@ def run():
 
     # Buttons
     btn_choisir = tk.Button(root, text="Input file", command=choisir_fichier, width=15)
-    btn2_choisir = tk.Button(root, text="Output file", command=choisir_fichier, width=15)
+    btn2_choisir = tk.Button(root, text="Output file", command=choisir_fichier2, width=15)
     btn_completer = tk.Button(root, text="Just do it!", command=completer_action, width=15)
 
     btn_choisir.pack(pady=10)
